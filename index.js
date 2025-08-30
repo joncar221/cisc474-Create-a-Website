f5d499188cdebdfe7240203a42ff2503ad89c12f
Jonathan Carney <143568658+joncar221@users.noreply.github.com>
2025-08-29 22:38:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const counter = document.getElementById("counter");
 let counterNum = 0;
 
 function changeCounter() {
+  console.log("hello");
   counterNum += 1;
   counter.textContent = counterNum;
 }
