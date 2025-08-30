b465e7a2ed271af0c9edb30307cf56cbff2ac089
Jonathan Carney <143568658+joncar221@users.noreply.github.com>
2025-08-29 22:35:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("button1");
 const counter = document.getElementById("counter");
-const counterNum = 0;
+let counterNum = 0;
 
 function changeCounter() {
   counterNum += 1;
